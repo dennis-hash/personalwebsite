4e6a11cc6081ad56fb440996afd6e2798d48d3c3
dennis <ndungudennis250@gmail.com>
2022-12-13 08:57:44 +0300

    fourth commit

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,18 +15,18 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
+        <meta name="description" content="Dennis' homepage" />
+        <meta name="author" content="Dennis Kariuki" />
         <meta name="author" content="craftzdog" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="Dennis Kariuki" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@craftzdog" />
         <meta name="twitter:creator" content="@craftzdog" />
         <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta property="og:site_name" content="Dennis Kariuki" />
+        <meta name="og:title" content="Dennis Kariuki" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://www.craftz.dog/card.png" />
         <title>Dennis Kariuki - Homepage</title>
