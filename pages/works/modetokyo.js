1d2c8c22f0497d343a16ef31da0d965986bba17f
dennis <ndungudennis250@gmail.com>
2022-12-14 07:53:53 +0300

    fifth commit

--- pages/works/modetokyo.js
+++ pages/works/modetokyo.js
@@ -15,57 +15,31 @@ const Work = () => (
   <Layout title="mode.tokyo">
     <Container>
       <Title>
-        mode.tokyo <Badge>2016</Badge>
+        Bank Portal <Badge>2021</Badge>
       </Title>
-      <P>The mode magazine for understanding to personally enjoy Japan.</P>
-      <P>
-        It is one of the projects launched at{' '}
-        <Link href="https://innolab.jp/" target="_blank">
-          Open Innovation Lab (電通イノラボ) <ExternalLinkIcon mx="2px" />
-        </Link>
-        , aiming to visualize the impact of media contents on the visit of
-        foreign high-intellect class to Japan, who are highly interested in
-        Japanese culture. I joined the project to build its mobile app for iOS
-        where you can read free articles of the magazine.
-      </P>
-      <P>
-        電子版としてiOSアプリを担当。日英仏対応。
-        きゅんくんやラブリーサマーちゃんなどを誌面で取り上げている。
-        本雑誌はISIDイノラボ様と角川アスキー総合研究所様の共同プロジェクトです。
-      </P>
-
+      <P>This demo, showcases a dummy banking portal. It includes: Django Rest Framework, Dialogflow, Angular, Socket.io and Kubernetes Engine, Cloud Builder, Pub/Sub, DLP API, NLP API, Google Cloud Storage, Cloud Functions and BigQuery.</P>
+      
       <List ml={4} my={4}>
         <ListItem>
           <Meta>Platform</Meta>
-          <span>iOS</span>
+          <span>Web</span>
         </ListItem>
         <ListItem>
           <Meta>Stack</Meta>
-          <span>Swift</span>
+          <span>Angular</span>
         </ListItem>
         <ListItem>
-          <Meta>Press release</Meta>
-          <Link href="https://www.isid.co.jp/english/news/release/2015/pdf/20150930_ModeTokyo.pdf">
-            ISID Starts a Research Project to Visualize Impact of Media Contents
-            on Visit of Foreign High-Intellect Class to Japan{' '}
-            <ExternalLinkIcon mx="2px" />
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Meta>Press release</Meta>
-          <Link href="https://innolab.jp/news/achievement/335">
-            メディアコンテンツが訪日行動に与える影響を可視化する研究プロジェクトを始動{' '}
-            <ExternalLinkIcon mx="2px" />
+          <Meta>Github</Meta>
+          <Link href="https://github.com/dennis-hash/Project2ndYear">
+            Bank Portal <ExternalLinkIcon mx="2px" />
           </Link>
         </ListItem>
       </List>
 
-      <WorkImage src="/images/works/modetokyo_eyecatch.png" alt="mode.tokyo" />
-      <SimpleGrid columns={2} gap={2}>
-        <WorkImage src="/images/works/modetokyo_01.png" alt="mode.tokyo" />
-        <WorkImage src="/images/works/modetokyo_02.png" alt="mode.tokyo" />
-      </SimpleGrid>
-      <WorkImage src="/images/works/modetokyo_03.jpg" alt="mode.tokyo" />
+      <WorkImage src="/images/works/bank1.png" alt="STYLY" />
+      <WorkImage src="/images/works/bank2.png" alt="STYLY" />
+      <WorkImage src="/images/works/bank3.png" alt="STYLY" />
+  
     </Container>
   </Layout>
 )
