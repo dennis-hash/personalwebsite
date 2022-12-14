2957405f6a99e6d06d9cf65590275fb90882b07f
dennis <ndungudennis250@gmail.com>
2022-12-14 10:40:29 +0300

    commit

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST={"../components/layouts/main.js -> ../voxel-dog":{"id":"../components/layouts/main.js -> ../voxel-dog","files":["static/chunks/components_voxel-dog_js.js"]}}
