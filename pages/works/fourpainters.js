06a14e51e827f9781a66c1f29df4bad3dcd71637
dennis <ndungudennis250@gmail.com>
2022-12-14 05:39:07 +0300

    fifth commit

--- pages/works/fourpainters.js
+++ pages/works/fourpainters.js
@@ -10,6 +10,7 @@ import {
   Center
 } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
+import Logo from '../../components/logo'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
