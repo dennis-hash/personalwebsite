923257475690f83ef11621d8fd4f15986b7ec9fb
dennis <ndungudennis250@gmail.com>
2022-12-19 23:55:57 +0300

    last commit

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -126,7 +126,7 @@ const VoxelDog = () => {
 export default VoxelDog*/
 
 import { useState, useEffect, useRef, useCallback } from 'react'
-import {Box, Spinner}from '@chakra-ui/react'
+import {Box, Spinner, useStatStyles}from '@chakra-ui/react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { loadGLTFModel } from '../lib/model'
@@ -138,9 +138,99 @@ function easeOutCirc(x){
 const VoxelDog = () => {
   const refContainer = useRef()
   const [loading,setLoading] = useState(true)
+  const [renderer, setRenderer] = useState()
+  const [_camera, setCamera] = useState()
+  const [target] = useState(new THREE.Vector3(-0.5,1.2,0))
+  const [initialCameraPosition] = useState(
+    new THREE.Vector3(
+      20 * Math.sin(0.2 * Math.PI),
+      10,
+      20 * Math.cos(0.2 * Math.PI)
+    )
+  )
+  const [scene] = useState(new THREE.Scene())
+  const [_controls, setControls] = useState()
+  /*disabe react-hooks */
+  useEffect(() => {
+    const { current: container } = refContainer
+    if (container && !renderer){
+      const scW = container.clientWidth
+      const scH = container.clientHeight
+
+      const renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
+      })
+      renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.setSize(scW, scH)
+      renderer.outputEncoding = THREE.sRGBEncoding
+      container.appendChild(renderer.domElement)
+      setRenderer(renderer)
+
+      const scale = scH * 0.005 + 4.8
+      const camera = new THREE.OrthographicCamera(
+        -scale,
+        scale,
+        scale,
+        -scale,
+        0.01,
+        50000
+      )
+      camera.position.copy(initialCameraPosition)
+      camera.lookAt(target)
+      setCamera(camera)
+
+      const ambientLight = new THREE.AmbientLight(0xcccccc, 1)
+      scene.add(ambientLight)
+
+      const controls = new OrbitControls(camera, renderer.domElement)
+      controls.autoRotate = true
+      setControls(controls)
+
+    }
+
+    loadGLTFModel(scene, '/dog.glb', {
+      receiveShadow: false,
+      castShadow: false
+    }).then(() => {
+      animate()
+      setLoading(false)
+    })
+    
+    let req = null
+    let frame = 0
+    const animate = () => {
+      req = requestAnimationFrame(animate)
+
+      frame = frame <= 100 ? frame + 1 : frame
+
+      if (frame <= 100) {
+        const p = initialCameraPosition
+        const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20
+
+        camera.position.y = 10
+        camera.position.x =
+          p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed)
+        camera.position.z =
+          p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed)
+        camera.lookAt(target)
+      } else {
+        controls.update()
+      }
+
+      renderer.render(scene, camera)
+    }
+    return () => {
+      cancelAnimationFrame(req)
+      //renderer.domElement.remove()
+      renderer.dispose()
+    }
   
 
-  return <Box ref={refContainer} className='voxel-dog' m="auto" 
+    },[])
+
+
+  return ( <Box ref={refContainer} className='voxel-dog' m="auto" 
   at={['-20px','-60px','-120px']}
   mb={['-40px','-140px','-200px']}
   w={[280,480,640]}
@@ -149,6 +239,6 @@ const VoxelDog = () => {
   {loading && (<Spinner
   size="xl" position="absolute" left="50%" top="50%" ml="calc(0px - var(--spinner-size)/2)" mt="calc(0px -var(--spinner-size))"/>)}
   </Box>
-
+  )
 }
 export default VoxelDog
